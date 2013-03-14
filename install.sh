672bbc05f7cf57e2724d2ccb9f69086a4effefe1
Mike McClenaghan <mike@sideline.ca>
2013-03-13 19:32:50 -0600

    Initial commit

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#                                                                  
+#                                                                  
+# _________   _...._            .----.     .----.   __.....__      
+# \        |.'      '-.          \    \   /    /.-''         '.    
+#  \        .'```'.    '.         '   '. /'   //     .-''"'-.  `.  
+#   \      |       \     \   __   |    |'    //     /________\   \ 
+#    |     |        |    |.:--.'. |    ||    ||                  | 
+#    |      \      /    ./ |   \ |'.   `'   .'\    .-------------' 
+#    |     |\`'-.-'   .' `" __ | | \        /  \    '-.____...---. 
+#    |     | '-....-'`    .'.''| |  \      /    `.             .'  
+#   .'     '.            / /   | |_  '----'       `''-...... -'    
+# '-----------'          \ \._,\ '/                                
+#                         `--'  `"                                 
+# 
+#
+#     This is the installation script for Pave.
+#     See the source: https://github.com/mm53bar/pave/
+#
+#     Install Pave by running this command:
+#     curl https://raw.github.com/mm53bar/pave/master/install.sh | sh
+
+# Set up the environment
+
+      set -e
+
+# Create the bin folder if it doesn't already exist
+
+      mkdir -p bin
+
+# Download the pave script
+
+      curl -s -L https://raw.github.com/mm53bar/pave/master/bin/pave -o bin/pave
+      
+# Downoad support files
+
+      curl -s -L https://raw.github.com/mm53bar/plow/master/.env.sample -o .env.sample
+
+# Set the permissions for the scripts
+
+      chmod +x bin/pave
+
+# All done
+
+      echo 'Installed!'
+      echo      
+      echo 'Be sure to update .env.sample. Then copy it to .env.production'
